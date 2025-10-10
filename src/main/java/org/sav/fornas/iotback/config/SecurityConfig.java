977bbae81d1a1cb1cb4961a30087b2237efa90c9
lyolek <lyolek@gmail.com>
2025-10-10 12:20:12 +0300

    додав перевірку авторизації

--- src/main/java/org/sav/fornas/iotback/config/SecurityConfig.java
+++ src/main/java/org/sav/fornas/iotback/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package org.sav.fornas.iotback.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,6 +11,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
 @EnableWebSecurity
@@ -21,12 +23,21 @@ public class SecurityConfig {
 				.csrf(AbstractHttpConfigurer::disable)
 				.authorizeHttpRequests(auth -> auth
 						.requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll()
-						.anyRequest().authenticated()
+						.anyRequest().hasRole("IOT_USER")
 				)
 				.oauth2ResourceServer(oauth2 -> oauth2
 						.jwt(jwt -> jwt
 								.jwtAuthenticationConverter(jwtAuthenticationConverter())
 						)
+				)
+				.exceptionHandling(ex -> ex
+						// користувач не авторизований → 401
+						.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+						// користувач авторизований, але не має ролі → 403
+						.accessDeniedHandler((request, response, accessDeniedException) -> {
+							response.setStatus(HttpStatus.FORBIDDEN.value());
+							response.getWriter().write("У вас немає доступу");
+						})
 				)
 				.build();
 	}
