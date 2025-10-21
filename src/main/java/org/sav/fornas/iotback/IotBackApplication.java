b1d158c4338970dcbd4456aac4b11f67fc108f0a
lyolek <lyolek@gmail.com>
2025-10-21 09:42:09 +0300

    Змінив час старту

--- src/main/java/org/sav/fornas/iotback/IotBackApplication.java
+++ src/main/java/org/sav/fornas/iotback/IotBackApplication.java
@@ -3,9 +3,11 @@ package org.sav.fornas.iotback;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Slf4j
 @SpringBootApplication
+@EnableScheduling
 public class IotBackApplication {
 
 	public static void main(String[] args) {
--- src/main/java/org/sav/fornas/iotback/service/schedule/HistoryAggregationService.java
+++ src/main/java/org/sav/fornas/iotback/service/schedule/HistoryAggregationService.java
@@ -1,6 +1,7 @@
 package org.sav.fornas.iotback.service.schedule;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.sav.fornas.iotback.repository.PortHistoryRepository;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -10,16 +11,20 @@ import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class HistoryAggregationService {
 
 	private final PortHistoryRepository repository;
 
-	// щодня о 01:00 ночі
-	@Scheduled(cron = "0 0 1 * * *")
+	@Scheduled(cron = "0 5 0 * * *")
 	public void aggregateYesterday() {
+		log.debug(">>> started");
 		LocalDate yesterday = LocalDate.now().minusDays(1);
 		LocalDateTime start = yesterday.atStartOfDay();
 		LocalDateTime end = yesterday.plusDays(1).atStartOfDay();
+
+		log.debug(">>> start: {} end: {}", start, end);
 		repository.aggregatePortHistory(start, end);
+		log.debug(">>> finished");
 	}
 }
