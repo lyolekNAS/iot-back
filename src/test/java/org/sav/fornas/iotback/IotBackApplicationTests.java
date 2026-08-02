6298372d31b945a95007b843e61658a404b31f09
Lyolek <lyolek@gmail.com>
2026-08-02 13:35:46 +0300

    Виправив тести

--- src/test/java/org/sav/fornas/iotback/IotBackApplicationTests.java
+++ src/test/java/org/sav/fornas/iotback/IotBackApplicationTests.java
@@ -2,8 +2,10 @@ package org.sav.fornas.iotback;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class IotBackApplicationTests {
 
 	@Test
